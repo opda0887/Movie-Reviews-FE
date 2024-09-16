2260ead68642c328b4c5a09dd36420b2d6e5e9e8
Kiwi_Lin <opda0887@gmail.com>
2024-09-16 17:35:54 +0800

    connect getAllMovies API

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // inorder for client HTTP requests to not be blocked by CORS
 export default axios.create({
-    baseURL: "https://9c96-103-106-239-104.ap.ngrok.io",
+    baseURL: "http://localhost:8080",
     headers: {
         "ngrok-skip-browser-warning": "true"
     }
